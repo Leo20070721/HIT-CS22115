a1e4941266abef5e52c2e3d6e71a77f28ec17636
Leo Anderson <Leo20070721@foxmail.com>
2025-09-21 16:25:10 +0800

    save launch.json

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hello World!')
+/*
+Welcome to HIT-CS22115 移动互联网应用开发实践
+*/
+
+/*
+let name = 'Alice'
+let age = 18
+let hello = `My name is ${name},I am ${age + 1} years old`
+console.log(hello)
+*/
+
+/*
+var a = 5
+if (a > 0){
+    var 棍斤拷 = 7
+}
+console.log(棍斤拷)
+*/
+
+/*
+let [a,b]=[5,7];
+[a,b]=[b,a];
+console.log(a);
+console.log(b);
+*/
+
+console.log(~4)
